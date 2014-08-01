8eac51198b9ca41ed69dadff0d9e15aa3c16971f
Rodrigo Ferreira <r.caneppele@gmail.com>
2014-08-01 13:26:43 -0300

    protegendo HomeController

--- src/main/java/br/com/rcaneppele/loja/controller/HomeController.java
+++ src/main/java/br/com/rcaneppele/loja/controller/HomeController.java
@@ -5,8 +5,10 @@ import javax.inject.Inject;
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Result;
+import br.com.rcaneppele.loja.infra.security.Security;
 
 @Controller
+@Security
 public class HomeController {
 
 	@Inject
